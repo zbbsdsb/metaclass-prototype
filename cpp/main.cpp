e4981b7cfc9caa8f7595dd8b2e029d0a39cee39a
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-05 06:52:49 +0800

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,1 +1,14 @@
+// main.cpp
+#include "grass.cpp"
+#include "water.cpp"
+
+int main() {
+    Grass grass;
+    Water water;
+
+    grass.displayAttributes();
+    water.displayAttributes();
+
+    return 0;
+}
 
