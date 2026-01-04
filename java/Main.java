7284f99158afb884fa498cb353cefbbffd40141e
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-05 06:49:48 +0800

    Update Main.java

--- java/Main.java
+++ java/Main.java
@@ -1,1 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Grass grass = new Grass();
+        Water water = new Water();
+
+        grass.displayAttributes();
+        water.displayAttributes();
+    }
+}
 
