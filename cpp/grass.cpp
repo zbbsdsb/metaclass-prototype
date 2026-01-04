004a1e2ffabbeb12654ee4b28358996ab903289c
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-04 22:21:02 +0800

    Implement Grass and Water classes with attributes

--- cpp/grass.cpp
+++ cpp/grass.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+#include <string>
+
+class Metaclass {
+public:
+    virtual void displayAttributes() = 0;
+};
+
+class Grass : public Metaclass {
+public:
+    void displayAttributes() override {
+        std::cout << "Grass Attributes:\n"
+                  << "Height: 10cm\n"
+                  << "Color: Green\n"
+                  << "Density: Medium\n"
+                  << "Growth Condition: Humid\n"
+                  << "Interaction: Can be trampled or cut\n";
+    }
+};
+
+class Water : public Metaclass {
+public:
+    void displayAttributes() override {
+        std::cout << "Water Attributes:\n"
+                  << "State: Liquid\n"
+                  << "Temperature: 0-100C\n"
+                  << "Flow: Yes\n"
+                  << "Evaporation: Yes\n"
+                  << "Freezing: Yes\n";
+    }
+};
+
+int main() {
+    Grass grass;
+    Water water;
+
+    grass.displayAttributes();
+    water.displayAttributes();
+
+    return 0;
+}
 
