b9f400cc6029184af9e1bd813d6e29a7509a8be2
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-05 06:47:33 +0800

    Update water.cpp

--- cpp/water.cpp
+++ cpp/water.cpp
@@ -1,1 +1,16 @@
+// water.cpp
+#include "metaclass.hpp"
+#include <iostream>
+
+class Water : public Metaclass {
+public:
+    void displayAttributes() const override {
+        std::cout << "Water Attributes:\n"
+                  << "State: Liquid\n"
+                  << "Temperature: 0-100C\n"
+                  << "Flow: Yes\n"
+                  << "Evaporation: Yes\n"
+                  << "Freezing: Yes\n";
+    }
+};
 
