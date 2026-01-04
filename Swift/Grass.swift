76a81afeff4ef0586ff448f9ab90b067a4b1c9df
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-05 06:54:05 +0800

    Add Grass class with displayAttributes method

--- Swift/Grass.swift
+++ Swift/Grass.swift
@@ -1,1 +1,11 @@
+class Grass: Metaclass {
+    func displayAttributes() {
+        print("Grass Attributes:")
+        print("Height: 10cm")
+        print("Color: Green")
+        print("Density: Medium")
+        print("Growth Condition: Humid")
+        print("Interaction: Can be trampled or cut")
+    }
+}
 
