1a7c5ac85f92422e4947cce7986c737a86a4939b
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-05 12:22:39 +0800

    Update water.rs

--- rust/water.rs
+++ rust/water.rs
@@ -1,1 +1,15 @@
+use crate::metaclass::Metaclass;
+
+pub struct Water;
+
+impl Metaclass for Water {
+    fn display_attributes(&self) {
+        println!("Water Attributes:");
+        println!("State: Liquid");
+        println!("Temperature: 0-100C");
+        println!("Flow: Yes");
+        println!("Evaporation: Yes");
+        println!("Freezing: Yes");
+    }
+}
 
