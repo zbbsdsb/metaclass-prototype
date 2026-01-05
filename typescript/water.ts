78469899c04b450f2d94f683ec7e605550c1f3d9
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-06 06:52:36 +0800

    Update water.ts

--- typescript/water.ts
+++ typescript/water.ts
@@ -1,1 +1,16 @@
+import { Metaclass } from "./metaclass";
+
+export class Water implements Metaclass {
+  readonly type = "water";
+
+  describe() {
+    return {
+      state: "liquid",
+      temperatureRange: "0–100°C",
+      flow: true,
+      evaporation: true,
+      freezing: true
+    };
+  }
+}
 
