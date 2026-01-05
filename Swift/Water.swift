6aab1f4da1cb735b9b0fb85f599abc320b7aebfd
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-05 12:24:03 +0800

    Add Water class with attributes display method

--- Swift/Water.swift
+++ Swift/Water.swift
@@ -1,1 +1,11 @@
+class Water: Metaclass {
+    func displayAttributes() {
+        print("Water Attributes:")
+        print("State: Liquid")
+        print("Temperature: 0-100C")
+        print("Flow: Yes")
+        print("Evaporation: Yes")
+        print("Freezing: Yes")
+    }
+}
 
