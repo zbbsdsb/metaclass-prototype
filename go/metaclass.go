9f6506376a748e546f6f1cf9a520fa2b97dfd082
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-06 12:08:12 +0800

    Update metaclass.go

--- go/metaclass.go
+++ go/metaclass.go
@@ -1,1 +1,35 @@
+package main
+
+import "fmt"
+
+type Metaclass interface {
+    Describe() map[string]interface{}
+}
+
+type Grass struct{}
+
+func (g Grass) Describe() map[string]interface{} {
+    return map[string]interface{}{
+        "height":  "10cm",
+        "color":   "green",
+        "density": "medium",
+    }
+}
+
+type Water struct{}
+
+func (w Water) Describe() map[string]interface{} {
+    return map[string]interface{}{
+        "state":      "liquid",
+        "temperature": "0-100C",
+        "flow":        true,
+    }
+}
+
+func main() {
+    objects := []Metaclass{Grass{}, Water{}}
+    for _, obj := range objects {
+        fmt.Println(obj.Describe())
+    }
+}
 
