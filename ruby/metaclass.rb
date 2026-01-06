590280df349ecf5ec20624d6db9a46a542267757
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-06 12:08:44 +0800

    Update metaclass.rb

--- ruby/metaclass.rb
+++ ruby/metaclass.rb
@@ -1,1 +1,32 @@
+class Metaclass
+  def describe
+    raise NotImplementedError, "Subclasses must implement the describe method"
+  end
+end
+
+class Grass < Metaclass
+  def describe
+    {
+      height: "10cm",
+      color: "green",
+      density: "medium"
+    }
+  end
+end
+
+class Water < Metaclass
+  def describe
+    {
+      state: "liquid",
+      temperature: "0-100C",
+      flow: true
+    }
+  end
+end
+
+grass = Grass.new
+water = Water.new
+
+puts grass.describe
+puts water.describe
 
